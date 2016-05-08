1b864430bd43d4a6d58e7fdd6f6b804c9dcb948a
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-08 19:41:57 +0100

    adds more fixtures for the 5 diff departure airports

--- test/index.test.js
+++ test/index.test.js
@@ -4,9 +4,16 @@ console.log(urls);
 var parse = require('../');
 
 describe('Parses the Query string of a Spies.dk url', function () {
-  it('Parse fixture', function (done) {
+  it('Parse departure date', function (done) {
     var q = parse(urls[0]);
     assert(q.QueryDepDate = '20160701');
+    done();
+  });
+  it('Parse departure date', function (done) {
+    urls.forEach(function (url) {
+      var q = parse(url);
+      console.log(q.QueryDepID);
+    });
     done();
   });
 });
